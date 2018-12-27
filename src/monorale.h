d1e164e4576e7cdaed029160f0d6a985ba179486
Chromaryu <knight.ryu12@gmail.com>
2018-12-27 18:37:52 +0900

    Initial Commit
    BadApple vita..?

---
+++ src/monorale.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+typedef struct {
+	uint32_t offset;
+	uint32_t cmdcnt;
+} __attribute__((packed)) monorale_frameinf;
+
+typedef struct {
+	uint32_t framecnt;
+	monorale_frameinf inf[0];
+} __attribute__((packed)) monorale_hdr;
+
+static inline size_t monorale_frames(monorale_hdr *hdr) {
+	return hdr->framecnt;
+}
+
+monorale_hdr *monorale_init(const char *path);
+int monoraleThread(SceSize args,void *argp);
