dd0b66a11c406e1c177d06f132a60e9c1ca6a4c6
Chromaryu <knight.ryu12@gmail.com>
2018-12-29 22:16:57 +0900

    Optimize monorale.c

--- src/monorale.c
+++ src/monorale.c
@@ -15,8 +15,7 @@
 #define printf LOG
 #define sceClibMemset memset // emulator aware.
 static SceUID fdata_memblock = 0;
-static SceUID fb_memblock_L = 0;
-static SceUID fb_memblock_R = 0;
+static SceUID fb_memblock = 0;
 
 void LOG(char* format, ...){
 	char str[512] = { 0 };
@@ -29,7 +28,6 @@ void LOG(char* format, ...){
 	SceUID fd;
 	fd = sceIoOpen(LOG_FILE, SCE_O_WRONLY | SCE_O_APPEND | SCE_O_CREAT, 0777);
 	sceIoWrite(fd, str, strlen(str));
-	sceIoWrite(fd, "\n", 1);
 	sceIoClose(fd);
 #undef sceClibPrintf
 	sceClibPrintf(str); // callback
@@ -52,19 +50,15 @@ static inline uint32_t monorale_framecnt(monorale_hdr *hdr, size_t frame)
 /* TODO: Add hardware filling */
 void monorale_doframe(monorale_hdr *hdr, size_t frame, uint32_t *fb)
 {
-	//printf("Doing Frame Job!\n");
 	uint32_t fill_val = 0;
 	uint16_t *cmds = monorale_frame(hdr, frame);
 	size_t cmdcnt = monorale_framecnt(hdr, frame);
-	//printf("%d\n",cmdcnt);
 	for (size_t i = 0; i < cmdcnt; i++) {
 		size_t len_px = cmds[i];
 		memset(fb, fill_val, len_px << 2);
-		//printf("memset\n");
 		fb += len_px;
 		fill_val ^= 0xFFFFFFFF; 
 	}
-	//printf("Frame %d render!\n",frame);
 }
 
 monorale_hdr *monorale_init(const char *path)
@@ -80,7 +74,6 @@ monorale_hdr *monorale_init(const char *path)
 	memBlockSize = (((uint32_t)stat.st_size & 0xFFFFF000)+0x1000);
 	sceClibPrintf("Need Allocate 0x%lXB\n",memBlockSize);
 	if(allocMemory(memBlockSize) < 0) {
-		// Alloc Failed!!!
 		sceClibPrintf("memory allocation failed???\n");
 		return NULL;
 	}
@@ -105,26 +98,18 @@ int allocMemory(uint32_t monolen) {
 	fbmOpt.attr = 4;
 	fbmOpt.alignment = 256*1024;
 	fb_memblock_L = sceKernelAllocMemBlock("FrameBuffer CDRAM L",SCE_KERNEL_MEMBLOCK_TYPE_USER_CDRAM_RW,0x200000,&fbmOpt);
-	sceClibPrintf("fb_memblockL 0x%x\n",fb_memblock_L);
-	fb_memblock_R = sceKernelAllocMemBlock("FrameBuffer CDRAM R",SCE_KERNEL_MEMBLOCK_TYPE_USER_CDRAM_RW,0x200000,&fbmOpt);
-	sceClibPrintf("fb_memblockR 0x%x\n",fb_memblock_R);
+	sceClibPrintf("fb_memblockL 0x%x\n",fb_memblock);
 	fdata_memblock = sceKernelAllocMemBlock("FrameData UMEM",SCE_KERNEL_MEMBLOCK_TYPE_USER_RW_UNCACHE,monolen, NULL);
 	sceClibPrintf("fdata_memblock 0x%x\n",fdata_memblock);
-	if(fb_memblock_L < 0) return fb_memblock_L;
-	if(fb_memblock_R < 0) return fb_memblock_R;
-	if(fdata_memblock < 0) {
-		return fdata_memblock;
-	}
+	if(fb_memblock < 0) return fb_memblock;
+	if(fdata_memblock < 0) return fdata_memblock;
 	return 0;
 }
 
 
 int monorale_deinit(){
-	if(fb_memblock_L > 0) sceKernelFreeMemBlock(fb_memblock_L);
-	if(fb_memblock_R > 0) sceKernelFreeMemBlock(fb_memblock_R);
-	if(fdata_memblock > 0) {
-		sceKernelFreeMemBlock(fdata_memblock);
-	}
+	if(fb_memblock > 0) sceKernelFreeMemBlock(fb_memblock);
+	if(fdata_memblock > 0) sceKernelFreeMemBlock(fdata_memblock);
 	return 0; // always success.
 }
 
@@ -132,26 +117,16 @@ int monorale_deinit(){
 
 int monoraleThread(SceSize args, void *argp) {
 	printf("I'm in.\n");
-	void* base[2];
-	sceKernelGetMemBlockBase(fb_memblock_L,&base[0]);
-	sceKernelGetMemBlockBase(fb_memblock_R,&base[1]);
-	SceDisplayFrameBuf bufL;
-	SceDisplayFrameBuf bufR;
-	memset(&bufL,0,sizeof(SceDisplayFrameBuf));
-	memset(&bufR,0,sizeof(SceDisplayFrameBuf));
-       	bufL.size = sizeof(SceDisplayFrameBuf);
-	bufL.base = base[0];
-	bufL.pitch = 640;
-	bufL.pixelformat = 0;
-	bufL.width = 640;
-	bufL.height = 368;
-	bufR = bufL;
-	bufR.base = base[1];
-
-
-	//SceDisplayFrameBuf bufR;
-	//buf.height = 640;
-	//sceDisplaySetFrameBuf(&buf,SCE_DISPLAY_SETBUF_IMMEDIATE);
+	void* base;
+	sceKernelGetMemBlockBase(fb_memblock,&base);
+	SceDisplayFrameBuf buf;
+	memset(&buf,0,sizeof(SceDisplayFrameBuf));
+       	buf.size = sizeof(SceDisplayFrameBuf);
+	buf.base = base[0];
+	buf.pitch = 640;
+	buf.pixelformat = 0;
+	buf.width = 640;
+	buf.height = 368;
 
 	int frame = 0;
 	monorale_hdr **tmp = (monorale_hdr**)argp;
@@ -162,18 +137,8 @@ int monoraleThread(SceSize args, void *argp) {
 	printf("baseaddrR=%p\n",base[1]);
 	SceDisplayFrameBuf* disp = NULL;
 	while(frame < monorale_frames(hdr)) {
-<<<<<<< Updated upstream
-		//disp = ((frame%2)==0)?&bufL:&bufR;
-		monorale_doframe(hdr,frame,bufL.base);
-		sceDisplaySetFrameBuf(&bufL,1);
-=======
-		printf("f:%d\n",frame);
-		monorale_doframe(hdr,frame,(uint16_t*)base);
-		if(sceDisplaySetFrameBuf(&buf,1)){
-			printf("Error on sceDisplaySetFrameBuf()\n");
-			break;
-		}
->>>>>>> Stashed changes
+		monorale_doframe(hdr,frame,buf.base);
+		sceDisplaySetFrameBuf(&buf,1)
 		frame++;
 		sceDisplayWaitVblankStart(); //problematic on Vita3K.
 	}
