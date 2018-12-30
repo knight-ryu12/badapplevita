c63532e9c3b5554c79b2d9e9a4b16a4e88c64c2a
Takuya Kousaka <kousakatakuya@gmail.com>
2018-12-30 12:24:00 +0800

    Atleast let the app run without audio for now

--- src/main.c
+++ src/main.c
@@ -16,16 +16,16 @@ int main(int argc, char *argv[]) {
 	if(port<0) printf("Open Audio Failed!!!");
 	
 	SceUID vid = sceKernelCreateThread("video thread", monoraleThread, 0x40, 0x10000, 0, 0, NULL);
-       	SceUID snd = sceKernelCreateThread("sound thread", soundThread,0x41,0x10000,0,0,NULL);
+       	//SceUID snd = sceKernelCreateThread("sound thread", soundThread,0x41,0x10000,0,0,NULL);
 	
 	if(vid < 0){printf("videoThread creation failed E:%x\n",vid); goto end;}
-	else if(snd < 0){ printf("soundThread creation failed E:%x\n",snd); goto end;}
+	//else if(snd < 0){ printf("soundThread creation failed E:%x\n",snd); goto end;}
 	else {
 		sceKernelStartThread(vid,sizeof(monorale_hdr),&hdr);
-		sceKernelStartThread(snd,sizeof(int),&port);
+		//sceKernelStartThread(snd,sizeof(int),&port);
 	}
 	sceKernelWaitThreadEnd(vid,0,0);
-	sceKernelWaitThreadEnd(snd,0,0);
+	//sceKernelWaitThreadEnd(snd,0,0);
 end:
 	monorale_deinit();
 	return 0;
