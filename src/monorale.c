a39c45aa3e0aceb3186648e432f1fe8910422081
Princess of Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2018-12-31 04:32:13 +0900

    Update monorale.c

--- src/monorale.c
+++ src/monorale.c
@@ -5,17 +5,20 @@
 #include <psp2/io/stat.h>
 #include <psp2/io/fcntl.h>
 
+#include "sound.h"
 #include "monorale.h"
 
 #define LOG_FILE "ux0:/data/monorale_video.log"
 
 #define DISPLAY_WIDTH 640
 #define DISPLAY_HEIGHT 368
-#define sceClibPrintf LOG
+//#define sceClibPrintf LOG
 #define printf LOG
 #define sceClibMemset memset // emulator aware.
 static SceUID fdata_memblock = 0;
 static SceUID fb_memblock = 0;
+
+int video_ready = 1;
 
 void LOG(char* format, ...){
 	char str[512] = { 0 };
@@ -29,7 +32,7 @@ void LOG(char* format, ...){
 	fd = sceIoOpen(LOG_FILE, SCE_O_WRONLY | SCE_O_APPEND | SCE_O_CREAT, 0777);
 	sceIoWrite(fd, str, strlen(str));
 	sceIoClose(fd);
-#undef sceClibPrintf
+//#undef sceClibPrintf
 	sceClibPrintf(str); // callback
 }
 
@@ -67,77 +70,137 @@ monorale_hdr *monorale_init(const char *path)
 	monorale_hdr *monorale_base = NULL;
 	SceIoStat stat;
 	uint32_t memBlockSize = 0;
+	SceUID fd = 0;
+
 	if(sceIoGetstat(path,&stat) < 0) {
 		return NULL;
 	}
+
 	sceClibPrintf("%s=%lldB\n",path,stat.st_size);
+
 	memBlockSize = (((uint32_t)stat.st_size & 0xFFFFF000)+0x1000);
+
 	sceClibPrintf("Need Allocate 0x%lXB\n",memBlockSize);
+
 	if(allocMemory(memBlockSize) < 0) {
 		sceClibPrintf("memory allocation failed???\n");
 		return NULL;
 	}
+
 	sceKernelGetMemBlockBase(fdata_memblock,(void**)&monorale_base);
-	//sceClibPrintf("memblock_base 0x%X",monorale_base);
-	SceUID fd = sceIoOpen(path, SCE_O_RDONLY, 0777);
+
+	fd = sceIoOpen(path, SCE_O_RDONLY, 0777);
 	if(fd < 0) {
 		sceClibPrintf("File Opening failed!???\n");
+		sceClibPrintf("Error code : 0x%X\n", fd);
 		return NULL;
 	}
 	sceIoRead(fd,monorale_base, stat.st_size);
 	sceIoClose(fd);
+
 	return monorale_base;
 	
 }
 
 int allocMemory(uint32_t monolen) {
 	sceClibPrintf("Allocating Memory right now.\n");
+
 	SceKernelAllocMemBlockOpt fbmOpt;
-	memset(&fbmOpt,0,sizeof(SceKernelAllocMemBlockOpt)); //Nullify
+
+	memset(&fbmOpt, 0, sizeof(SceKernelAllocMemBlockOpt)); //Nullify
+
 	fbmOpt.size = sizeof(fbmOpt);
 	fbmOpt.attr = 4;
 	fbmOpt.alignment = 256*1024;
-	fb_memblock = sceKernelAllocMemBlock("FrameBuffer CDRAM",SCE_KERNEL_MEMBLOCK_TYPE_USER_CDRAM_RW,0x200000,&fbmOpt);
-	sceClibPrintf("fb_memblockL 0x%x\n",fb_memblock);
-	fdata_memblock = sceKernelAllocMemBlock("FrameData UMEM",SCE_KERNEL_MEMBLOCK_TYPE_USER_RW_UNCACHE,monolen, NULL);
-	sceClibPrintf("fdata_memblock 0x%x\n",fdata_memblock);
-	if(fb_memblock < 0) return fb_memblock;
-	if(fdata_memblock < 0) return fdata_memblock;
+
+	fb_memblock = sceKernelAllocMemBlock("FrameBuffer CDRAM", SCE_KERNEL_MEMBLOCK_TYPE_USER_CDRAM_RW, 0x200000, &fbmOpt);
+
+	sceClibPrintf("fb_memblockL 0x%x\n", fb_memblock);
+
+	if(fb_memblock < 0){
+		return fb_memblock;
+	}
+
+	fdata_memblock = sceKernelAllocMemBlock("FrameData UMEM", SCE_KERNEL_MEMBLOCK_TYPE_USER_RW_UNCACHE, monolen, NULL);
+
+	sceClibPrintf("fdata_memblock 0x%x\n", fdata_memblock);
+
+	if(fdata_memblock < 0){
+		return fdata_memblock;
+	}
+
 	return 0;
 }
 
 
 int monorale_deinit(){
-	if(fb_memblock > 0) sceKernelFreeMemBlock(fb_memblock);
-	if(fdata_memblock > 0) sceKernelFreeMemBlock(fdata_memblock);
+	printf("monorale_deinit()\n");
+
+	if(fb_memblock > 0){
+		sceKernelFreeMemBlock(fb_memblock);
+	}
+
+	if(fdata_memblock > 0){
+		sceKernelFreeMemBlock(fdata_memblock);
+	}
+
 	return 0; // always success.
 }
 
+int VideoReady(){
+	printf("VideoReady()\n");
+	while(video_ready){
+	}
 
+	printf("VideoReady() end\n");
+
+	return 0;
+}
+
+int VideoPlayGo(){
+	printf("VideoPlayGo()\n");
+	video_ready = 0;
+
+	return 0;
+}
 
 int monoraleThread(SceSize args, void *argp) {
-	printf("I'm in.\n");
-	void* base;
-	sceKernelGetMemBlockBase(fb_memblock,&base);
+
+	uint32_t frame = 0;
+	uint32_t all_frame = 0;
+
 	SceDisplayFrameBuf buf;
-	memset(&buf,0,sizeof(SceDisplayFrameBuf));
+
+	monorale_hdr **tmp = NULL;
+	monorale_hdr *hdr = NULL;
+
+	printf("I'm in.\n");
+
+	memset(&buf, 0, sizeof(SceDisplayFrameBuf));
+
+	sceKernelGetMemBlockBase(fb_memblock, &buf.base);
+
        	buf.size = sizeof(SceDisplayFrameBuf);
-	buf.base = base;
 	buf.pitch = 640;
 	buf.pixelformat = 0;
 	buf.width = 640;
 	buf.height = 368;
 
-	int frame = 0;
-	monorale_hdr **tmp = (monorale_hdr**)argp;
-	monorale_hdr *hdr = *tmp;
+	tmp = (monorale_hdr**)argp;
+	hdr = *tmp;
+
+	all_frame = monorale_frames(hdr);
+
 	printf("Render Lv2\n");
-	printf("%d frames\n",monorale_frames(hdr));
-	printf("baseaddr=%p\n",base);
-	SceDisplayFrameBuf* disp = NULL;
-	while(frame < monorale_frames(hdr)) {
-		monorale_doframe(hdr,frame,buf.base);
-		sceDisplaySetFrameBuf(&buf,1);
+	printf("%d frames\n", all_frame);
+	printf("baseaddr=%p\n", buf.base);
+
+	VideoPlayGo();
+	SoundReady();
+
+	while(frame < all_frame) {
+		monorale_doframe(hdr, frame, buf.base);
+		sceDisplaySetFrameBuf(&buf, 1);
 		frame++;
 		sceDisplayWaitVblankStart(); //problematic on Vita3K.
 	}
